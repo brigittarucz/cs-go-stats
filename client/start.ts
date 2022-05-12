aa38d9ac058c67676cd261ab9c3d3087f1d32224
BR <brigitta.rucz@yahoo.com>
2022-05-12 10:22:31 +0200

    Add images, create HTML skeleton, add header and general style with typography

--- client/start.ts
+++ client/start.ts
@@ -2,5 +2,5 @@ console.log("I am the client");
 
 (async () => {
     const res = await fetch("http://localhost:3000/getStats");
-    console.log(await res.text());
+    console.log(await res.json());
 })();
--- public/start.js
+++ public/start.js
@@ -1,6 +1,6 @@
 console.log("I am the client");
 (async () => {
     const res = await fetch("http://localhost:3000/getStats");
-    console.log(await res.text());
+    console.log(await res.json());
 })();
 //# sourceMappingURL=start.js.map
